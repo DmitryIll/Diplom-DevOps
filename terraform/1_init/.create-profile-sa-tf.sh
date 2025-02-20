415ef3e4a339956fcaded88a3483a216820dbd20
DmitryIll <dnillarionov@gmail.com>
2025-02-20 19:46:04 +0300

    1

---
+++ terraform/1_init/.create-profile-sa-tf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yc config profile create sa-tf
+yc config set organization-id bpf8g5ujsvrh7kl1dtfa
+yc config set cloud-id b1gb78r3d0luhtuca6rh
+yc config set folder-id b1gor4f0e12rfi2v1drt
+yc config set service-account-key .key.json
