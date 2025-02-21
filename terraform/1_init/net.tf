48b1255481d3d8df4c79a8b809cec5ea96818872
DmitryIll <dnillarionov@gmail.com>
2025-02-21 20:38:54 +0300

    1

--- terraform/1_init/net.tf
+++ terraform/1_init/net.tf
@@ -8,6 +8,11 @@ resource "yandex_resourcemanager_cloud_iam_member" "vpc_admin_prod" {
   member = "serviceAccount:${yandex_iam_service_account.sa_tf.id}"
 }
 
+resource "yandex_resourcemanager_cloud_iam_member" "vpc_user_prod" {
+  cloud_id = module.init.cloud_id #var.cloud_prod_id 
+  role     = "vpc.user"
+  member = "serviceAccount:${yandex_iam_service_account.sa_tf.id}"
+}
 
 resource "yandex_resourcemanager_cloud_iam_member" "cloud_resource_manager_admin_prod" {
   cloud_id = module.init.cloud_id #var.cloud_prod_id 
