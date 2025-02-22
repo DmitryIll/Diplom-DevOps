778b1846a69949faa06e1aaa84201a713788d05c
DmitryIll <dnillarionov@gmail.com>
2025-02-22 08:45:07 +0300

    1

--- terraform/1_init/nat.tf
+++ terraform/1_init/nat.tf
@@ -38,4 +38,11 @@ resource "yandex_compute_instance" "nat" {
   }
 }
 
+output "nat_external_ip" {
+   value="${yandex_compute_instance.nat.network_interface.0.nat_ip_address}"
+}
+
+output "nat_internal_ip" {
+   value="${yandex_compute_instance.nat.network_interface.0.ip_address}"
+}
 
