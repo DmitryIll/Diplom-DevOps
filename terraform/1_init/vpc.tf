6572f5db00394c8190d74fe1062bbc2e04486b6f
DmitryIll <dnillarionov@gmail.com>
2025-02-22 07:59:47 +0300

    1

--- terraform/1_init/vpc.tf
+++ terraform/1_init/vpc.tf
@@ -122,12 +122,12 @@ resource "yandex_vpc_subnet" "private_a" {
 }
 
 
-resource "yandex_vpc_route_table" "rt_priv" {
-  name       = "rt-priv"
-  network_id = "${yandex_vpc_network.prod_net.id}"
+# resource "yandex_vpc_route_table" "rt_priv" {
+#   name       = "rt-priv"
+#   network_id = "${yandex_vpc_network.prod_net.id}"
 
-  static_route {
-    destination_prefix = "0.0.0.0/0"
-    next_hop_address   = yandex_compute_instance.nat.network_interface.0.ip_address #yandex_vpc_gateway.nat_gateway.id
-  }
-}
+#   static_route {
+#     destination_prefix = "0.0.0.0/0"
+#     next_hop_address   = yandex_compute_instance.nat.network_interface.0.ip_address #yandex_vpc_gateway.nat_gateway.id
+#   }
+# }
