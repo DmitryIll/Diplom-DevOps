4f574661180a2d6a7971a39e319b20eb5bc2172b
DmitryIll <dnillarionov@gmail.com>
2025-02-22 08:01:27 +0300

    1

--- terraform/1_init/vpc.tf
+++ terraform/1_init/vpc.tf
@@ -113,13 +113,13 @@ resource "yandex_vpc_subnet" "public_a" {
 #   network_id     = "${yandex_vpc_network.prod-net.id}"
 # }
 
-resource "yandex_vpc_subnet" "private_a" {
-  name           = "private-a"
-  zone           = "ru-central1-a"
-  v4_cidr_blocks = ["10.2.1.0/24"]
-  network_id     = "${yandex_vpc_network.prod_net.id}"
-  route_table_id = yandex_vpc_route_table.rt_priv.id
-}
+# resource "yandex_vpc_subnet" "private_a" {
+#   name           = "private-a"
+#   zone           = "ru-central1-a"
+#   v4_cidr_blocks = ["10.2.1.0/24"]
+#   network_id     = "${yandex_vpc_network.prod_net.id}"
+#   route_table_id = yandex_vpc_route_table.rt_priv.id
+# }
 
 
 # resource "yandex_vpc_route_table" "rt_priv" {
