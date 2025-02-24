2169646e29c1e4f84a98a8e8ae1c252a6a2b6d29
DmitryIll <dnillarionov@gmail.com>
2025-02-24 09:11:57 +0300

    1

--- terraform/1_init/prod_k8.tf
+++ terraform/1_init/prod_k8.tf
@@ -125,6 +125,7 @@ module "kube" {
 #   cluster_id = "some_k8s_cluster_id"
 # }
 
-# output "cluster_external_v4_endpoint" {
-#   value = "${data.yandex_kubernetes_cluster.my_cluster.master.0.external_v4_endpoint}"
-# }
+output "kube_cluster_id" {
+  value = module.kube.cluster_id
+}
+
